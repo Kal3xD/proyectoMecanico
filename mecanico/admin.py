9575cad3683507dc763e6584bed6e2dce560ba7b
Kal3xD <matias.arancibia.ap3@gmail.com>
2023-06-01 14:08:52 -0400

    Creacion BBDD Mecanico y genero, superusuarios creados.

--- mecanico/admin.py
+++ mecanico/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Mecanico, Genero
 
 # Register your models here.
+
+admin.site.register(Mecanico)
+admin.site.register(Genero)
--- mecanico/models.py
+++ mecanico/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Mecanico(models.Model):
+    rut              = models.CharField(primary_key = True, max_length=10)
+    nombre           = models.CharField(blank=False, null=False, max_length=50)
+    ap_paterno       = models.CharField(blank=False, null=False ,max_length=50)
+    ap_materno       = models.CharField(blank=False, null=False, max_length=50)
+    fecha_nacimiento = models.DateField(blank=False, null=False)   
+    id_genero        = models.ForeignKey('Genero',on_delete=models.CASCADE, db_column='idGenero')
+    telefono         = models.CharField(max_length=45)
+    email            = models.EmailField(unique=True, max_length=100, blank=True, null=True)
+    direccion        = models.CharField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return str(self.nombre)+" "+str(self.ap_paterno)+ " " +str(self.ap_materno)   
+
+
+
+
+class Genero(models.Model):
+    id_genero  = models.AutoField(db_column='idGenero', primary_key=True) 
+    genero     = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.genero)
